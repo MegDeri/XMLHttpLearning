9b76de9406ef3bbd135b2a617b578edb44a7d656
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-04-17 20:36:57 +0300

    Add nested fetch to fetch.js

--- fetch.js
+++ fetch.js
@@ -4,12 +4,22 @@ fetch('https://pokeapi.co/api/v2/berry/cheri')
         if (!response.ok)
             throw new Error(`Error!!!!!!!! It is not status 200 is: ${response.status}`);
 
-        response.json().then(data => {
-            for (let elem of data.flavors) {
-                console.log(elem.flavor.name);
-            }
-        });
+        return response.json()
 
+    })
+    .then(data => {
+        const flavUrl = data.flavors[0].flavor.url;
+        return fetch(flavUrl);
+    })
+    .then((response) => {
+        if (!response.ok)
+            throw new Error(`Error!!!!!!!! It is not status 200 is: ${response.status}`);
+
+        return response.json()
+
+    })
+    .then(data => {
+        console.log(data);
     })
     .catch(err => {
         console.log('Something went wrong!');
