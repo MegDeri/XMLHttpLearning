e7afe53bdffab0e2cc6230146e1037b3776877b8
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-04-17 20:12:12 +0300

    Modify fetch.js

--- fetch.js
+++ fetch.js
@@ -1,27 +1,17 @@
-const firstReq = new XMLHttpRequest();
 
+fetch('https://pokeapi.co/api/v2/berry/cheri')
+    .then((response) => {
+        if (!response.ok)
+            throw new Error(`Error!!!!!!!! It is not status 200 is: ${response.status}`);
 
-firstReq.addEventListener('load', function () {
-    console.log('FIRST REQUEST WORKED!!!');
-    const data = JSON.parse(this.responseText);
-    const filmURL = data.flavors[0].flavor.url;
-    console.log(filmURL)
-    const filmReq = new XMLHttpRequest();
-    filmReq.addEventListener('load', function () {
-        console.log('SECOND REQUEST WORKED!!!');
-        console.log(this);
-        const filmData = JSON.parse(this.responseText);
-        console.log(filmData.berries);
-    });
-    filmReq.addEventListener('error', function (e) {
-        console.log('ERROR!!', e);
-    });
-    filmReq.open('GET', filmURL);
-    filmReq.send();
-});
-firstReq.addEventListener('error', (e) => {
-    console.log('ERROR!!!!!!');
-});
-firstReq.open('GET', 'https://pokeapi.co/api/v2/berry/cheri');
-firstReq.send();
-console.log('Request Sent!');
+        response.json().then(data => {
+            for (let elem of data.flavors) {
+                console.log(elem.flavor.name);
+            }
+        });
+
+    })
+    .catch(err => {
+        console.log('Something went wrong!');
+        console.log(err);
+    })
